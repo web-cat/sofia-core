c49bbc2b500364a38aeeef94b44fbe16e9a3a749
Tony Allevato <allevato@vt.edu>
2012-11-06 14:14:45 -0500

    Fixed spacing.

--- src/sofia/internal/events/MotionEventDispatcher.java
+++ src/sofia/internal/events/MotionEventDispatcher.java
@@ -7,50 +7,50 @@ import android.view.MotionEvent;
 //-------------------------------------------------------------------------
 /**
  * TODO document
- * 
+ *
  * @author  Tony Allevato
  * @version 2012.10.24
  */
 public class MotionEventDispatcher extends EventDispatcher
 {
-	//~ Constructors ..........................................................
+    //~ Constructors ..........................................................
 
-	// ----------------------------------------------------------
-	public MotionEventDispatcher(String method)
-	{
-		super(method);
-	}
+    // ----------------------------------------------------------
+    public MotionEventDispatcher(String method)
+    {
+        super(method);
+    }
 
 
-	//~ Protected methods .....................................................
+    //~ Protected methods .....................................................
 
-	// ----------------------------------------------------------
-	@Override
-	protected List<MethodTransformer> lookupTransformers(
-			Object receiver, List<Class<?>> argTypes)
-	{
-		List<MethodTransformer> descriptors =
-				super.lookupTransformers(receiver, argTypes);
+    // ----------------------------------------------------------
+    @Override
+    protected List<MethodTransformer> lookupTransformers(
+            Object receiver, List<Class<?>> argTypes)
+    {
+        List<MethodTransformer> descriptors =
+                super.lookupTransformers(receiver, argTypes);
 
-		xyTransformer.addIfSupportedBy(receiver, descriptors);
+        xyTransformer.addIfSupportedBy(receiver, descriptors);
 
-		return descriptors;
-	}
-	
+        return descriptors;
+    }
 
-	// ----------------------------------------------------------
-	/**
-	 * Transforms an event with signature (MouseEvent event) to one with
-	 * signature (float x, float y).
-	 */
-	private MethodTransformer xyTransformer =
-			new MethodTransformer(float.class, float.class)
-	{
-		// ----------------------------------------------------------
-		protected Object[] transform(Object... args)
-		{
-			MotionEvent e = (MotionEvent) args[0];
-			return new Object[] { (float) e.getX(), (float) e.getY() };
-		}			
-	};
+
+    // ----------------------------------------------------------
+    /**
+     * Transforms an event with signature (MouseEvent event) to one with
+     * signature (float x, float y).
+     */
+    private MethodTransformer xyTransformer =
+            new MethodTransformer(float.class, float.class)
+    {
+        // ----------------------------------------------------------
+        protected Object[] transform(Object... args)
+        {
+            MotionEvent e = (MotionEvent) args[0];
+            return new Object[] { (float) e.getX(), (float) e.getY() };
+        }
+    };
 }
