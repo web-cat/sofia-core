c78b405ceabff65f59ef48ecd467dea05c5f330e
Tony Allevato <allevato@vt.edu>
2012-12-13 18:58:25 -0500

    Fixed stupid bug in OptionalEventDispatcher that caused events to be dispatched twice.

--- src/sofia/internal/events/OptionalEventDispatcher.java
+++ src/sofia/internal/events/OptionalEventDispatcher.java
@@ -21,91 +21,93 @@ import java.util.List;
  * transformations, then you should create your own subclass of
  * {@link EventDispatcher} to contain that logic.
  * </p>
- * 
+ *
  * @author  Tony Allevato
  * @version 2012.11.01
  */
 public class OptionalEventDispatcher extends EventDispatcher
 {
-	//~ Fields ................................................................
+    //~ Fields ................................................................
 
-	private int minimumArgCount;
-
-
-	//~ Constructors ..........................................................
-
-	// ----------------------------------------------------------
-	/**
-	 * Creates an {@code OptionalEventDispatcher} where all of the arguments
-	 * are optional.
-	 * 
-	 * @param method the name of the method to invoke
-	 */
-	public OptionalEventDispatcher(String method)
-	{
-		this(method, 0);
-	}
-
-	
-	// ----------------------------------------------------------
-	/**
-	 * Creates an {@code OptionalEventDispatcher} that must have at least the
-	 * specified number of arguments, but where those after that are optional.
-	 * 
-	 * @param method the name of the method to invoke
-	 * @param minimumArgCount the minimum number of arguments that a handler
-	 *     method must take in order to match
-	 */
-	public OptionalEventDispatcher(String method, int minimumArgCount)
-	{
-		super(method);
-		
-		this.minimumArgCount = minimumArgCount;
-	}
+    private int minimumArgCount;
 
 
-	//~ Protected methods .....................................................
+    //~ Constructors ..........................................................
 
-	// ----------------------------------------------------------
-	@Override
-	protected List<MethodTransformer> lookupTransformers(
-			Object receiver, List<Class<?>> argTypes)
-	{
-		List<MethodTransformer> descriptors =
-				super.lookupTransformers(receiver, argTypes);
+    // ----------------------------------------------------------
+    /**
+     * Creates an {@code OptionalEventDispatcher} where all of the arguments
+     * are optional.
+     *
+     * @param method the name of the method to invoke
+     */
+    public OptionalEventDispatcher(String method)
+    {
+        this(method, 0);
+    }
 
-		Class<?>[] argTypeArray = new Class<?>[argTypes.size()];
-		argTypes.toArray(argTypeArray);
 
-		for (int i = argTypes.size(); i >= minimumArgCount; i--)
-		{
-			final int thisCount = i;
+    // ----------------------------------------------------------
+    /**
+     * Creates an {@code OptionalEventDispatcher} that must have at least the
+     * specified number of arguments, but where those after that are optional.
+     *
+     * @param method the name of the method to invoke
+     * @param minimumArgCount the minimum number of arguments that a handler
+     *     method must take in order to match
+     */
+    public OptionalEventDispatcher(String method, int minimumArgCount)
+    {
+        super(method);
 
-			MethodTransformer reverseTransformer =
-					new MethodTransformer(
-							firstN(argTypeArray, thisCount, Class.class))
-			{
-				protected Object[] transform(Object... args)
-				{
-					return firstN(args, thisCount, Object.class);
-				}			
-			};
+        this.minimumArgCount = minimumArgCount;
+    }
 
-			reverseTransformer.addIfSupportedBy(receiver, descriptors);
-		}
 
-		return descriptors;
-	}
+    //~ Protected methods .....................................................
 
-	
-	//~ Private methods .......................................................
+    // ----------------------------------------------------------
+    @Override
+    protected List<MethodTransformer> lookupTransformers(
+            Object receiver, List<Class<?>> argTypes)
+    {
+        List<MethodTransformer> descriptors =
+                super.lookupTransformers(receiver, argTypes);
 
-	// ----------------------------------------------------------
-	@SuppressWarnings("unchecked")
-	private static <T> T[] firstN(T[] array, int n, Class<T> klass)
-	{
-		T[] firstN = (T[]) Array.newInstance(klass, n);
-		System.arraycopy(array, 0, firstN, 0, n);
-		return firstN;
-	}
+        Class<?>[] argTypeArray = new Class<?>[argTypes.size()];
+        argTypes.toArray(argTypeArray);
+
+        // We start at size - 1 because the superclass implementation handles
+        // the exact match.
+        for (int i = argTypes.size() - 1; i >= minimumArgCount; i--)
+        {
+            final int thisCount = i;
+
+            MethodTransformer transformer =
+                    new MethodTransformer(
+                            firstN(argTypeArray, thisCount, Class.class))
+            {
+                protected Object[] transform(Object... args)
+                {
+                    return firstN(args, thisCount, Object.class);
+                }
+            };
+
+            transformer.addIfSupportedBy(receiver, descriptors);
+        }
+
+        return descriptors;
+    }
+
+
+    //~ Private methods .......................................................
+
+    // ----------------------------------------------------------
+    @SuppressWarnings("unchecked")
+    private static <T> T[] firstN(T[] array, int n, Class<T> klass)
+    {
+        T[] firstN = (T[]) Array.newInstance(klass, n);
+        System.arraycopy(array, 0, firstN, 0, n);
+        return firstN;
+    }
 }
